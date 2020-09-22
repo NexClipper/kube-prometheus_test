7f74b7024e41fc917479b4111e1633528077c587
SKYul <folimworld@gmail.com>
2020-09-22 16:32:44 +0900

    Update chktest.sh

--- chktest.sh
+++ chktest.sh
@@ -22,7 +22,7 @@ do
         echo "count : $ruzzz -> Status : $running"
         running=$(kubectl get pods  -n monitoring |grep -v NAME| grep -v Running | wc -l)
         sleep 3
-        if [ $ruzzz == "10" ]; then echo "failed. restart plz."; exit 1; fi
+        if [ $ruzzz == "99" ]; then echo "failed. restart plz."; exit 1; fi
 done
 echo "Done!"
 }
