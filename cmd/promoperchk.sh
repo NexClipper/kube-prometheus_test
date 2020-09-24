da4beae6998bee8ace621db80c5109edcb77b8b8
SKYul <folimworld@gmail.com>
2020-09-24 14:45:33 +0900

    Update promoperchk.sh

--- cmd/promoperchk.sh
+++ cmd/promoperchk.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 namespacechk=$(kubectl get ns monitoring|grep -v "NAME"|wc -l)
-promoperchk=$(kubectl get all -A|grep prometheus-operator|wc -l)
+#promoperchk=$(kubectl get all -A|grep prometheus-operator|wc -l)
+promoperchk=$(kubectl get svc -A|grep prometheus-operator|wc -l)
 if [ $namespacechk -gt 0 ]|| [ $promoperchk -gt 0 ]; then
         if [ $namespacechk -ne 0 ]; then echo "Namespace >> $(kubectl get ns monitoring|grep -v "NAME") <<"; fi
         if [ $promoperchk -ne 0 ]; then  echo "prometheus-operator find"; fi
